0675db2f1615eb8d4d4cabed67e7c9ecf11b2651
ahmed-saifulla <ahsaifulla@gmail.com>
2020-08-07 11:14:54 +0530

    Basic Routing

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ const init = async () => {
         }
     });
 
+    // Different Route 
+    server.route({
+        method: 'GET',
+        path: '/dashboard',         // e.g: http://localhost:8000/dashboard
+        handler: (request, h) => {
+            return '<h2> Dashboard </h2>';
+        }
+    });
+
+    // Dynamic Route 
+    server.route({
+        method: 'GET',
+        path: '/dashboard/{username}',  // e.g: http://localhost:8000/dashboard/saif
+        handler: (request, h) => {
+            return `<h2> Hello, ${request.params.username} </h2>`;
+        }
+    });
+
     await server.start();
     console.log('Server running on %s', server.info.uri);
 };
